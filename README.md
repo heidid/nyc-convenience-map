1192ab9ae3d2a52aa8b7e1c3625683531573dc14
Heidi Dong <dong.heidi@gmail.com>
2025-03-12 22:58:45 -0400

    create basic map

---
+++ README.md
@@ -0,0 +1,7 @@
+# living conveniently in nyc
+if you wanted to live in NYC and you were super lazy and wanted to be able to walk to Target and Trader Joe's within 5 minutes, where could you live?
+
+## data sources
+* coordinates of Target and Trader Joe's locations mostly from OpenStreetMap, a few from Bing Maps
+* isochrones from Mapbox Isochrone API
+* subway lines from [chriswhong/mapboxgl-nyc-subway](https://github.com/chriswhong/mapboxgl-nyc-subway/tree/master)
---
+++ map.js
@@ -0,0 +1,101 @@
+mapboxgl.accessToken =
+      "pk.eyJ1IjoiZW5oZGxlc3MiLCJhIjoiY2ttbzFrbGYyMDJkdTJ2cmtxcmpsMDBqNyJ9.exkUgXdwQ0bYXxrGS1L73A";
+
+const TARGET = TARGET_LOCATIONS.find(loc => loc.name === "Manhattan East Village")
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: "mapbox://styles/mapbox/light-v10",
+  center: [TARGET.lon, TARGET.lat],
+  zoom: 13
+});
+
+
+map.on("load", () => {
+  map.addLayer({
+    id: "isoLayer",
+    type: "fill",
+    source: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: TARGET_LOCATIONS.flatMap(loc => loc.isochrone.features)
+      }
+    },
+    paint: {
+      "fill-color": "red",
+      "fill-opacity": 0.3
+    }
+  });
+
+  map.addLayer({
+    id: "traderJoesIsochrones",
+    type: "fill",
+    source: {
+      type: "geojson",
+      data: {
+        type: "FeatureCollection",
+        features: TRADER_JOES_LOCATIONS.flatMap(loc => loc.isochrone.features)
+      },
+    },
+    paint: {
+      "fill-color": "blue",
+      "fill-opacity": 0.3
+    }
+  });
+});
+
+TARGET_LOCATIONS.forEach((place) =>
+  new mapboxgl.Marker({ color: "red" })
+    .setLngLat([place.lon, place.lat])
+    .addTo(map)
+);
+
+TRADER_JOES_LOCATIONS.forEach((place) =>
+  new mapboxgl.Marker({ color: "blue" })
+    .setLngLat([place.lon, place.lat])
+    .addTo(map)
+);
+
+
+// SUBWAY LINES
+// from https://github.com/chriswhong/mapboxgl-nyc-subway/blob/master/js/scripts.js
+map.on('style.load', () => {
+    // add geojson sources for subway routes and stops
+  map.addSource('nyc-subway-routes', {
+    type: 'geojson',
+    data: 'subway-data/nyc-subway-routes.geojson'
+  });
+
+  map.addSource('nyc-subway-stops', {
+    type: 'geojson',
+    data: 'subway-data/nyc-subway-stops.geojson'
+  });
+
+  // add layers by iterating over the styles in the array defined in subway-layer-styles.js
+  subwayLayerStyles.forEach((style) => {
+    map.addLayer(style)
+  })
+});
+
+
+
+// FETCH DATA
+// async function getIso(lon, lat, id) {
+//   const urlBase = "https://api.mapbox.com/isochrone/v1/mapbox/";
+//   const profile = "walking";
+//   const minutes = 5;
+//   const query = await fetch(
+//     `${urlBase}${profile}/${lon},${lat}?contours_minutes=${minutes}&polygons=true&access_token=${mapboxgl.accessToken}`,
+//     { method: "GET" }
+//   );
+//   const data = await query.json();
+//   // console.log(data);
+//   return data;
+// }
+// Promise.all(TRADER_JOES_LOCATIONS.map(loc => getIso(loc.lon, loc.lat)))
+//   .then((values) => {
+//     for (let i = 0; i < TRADER_JOES_LOCATIONS.length; i++) {
+//       TRADER_JOES_LOCATIONS[i].isochrone = values[i]
+//     }
+//     console.log(TRADER_JOES_LOCATIONS)
+//   })
